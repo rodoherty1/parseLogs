e9b0feeac05b12cd9276ceaa5674b4082f4349b9
Rob O'D <rodoherty1@gmail.com>
2013-07-31 18:17:46 +0100

    .gitignore is now working

--- logs/4-extractZippedTSVFilesFromDataToDisk3.sh
+++
@@ -1,5 +0,0 @@
-for f in `/opt/pcf/analytics/hadoop/bin/hadoop dfs -ls /data | awk '{print $8}'`; do
-        filename=`basename $f`
-        /opt/pcf/analytics/hadoop/bin/hadoop dfs -get $f - | gzip -c | ssh adaptive@10.10.9.209 "cat > /hdfs/disk3/SyniverseZippedTsvFiles/$filename.gz"
-done
-
